a9ed626f656f8769664d4bebc9f6897fd0bc324d
Sergey Berdnikov <30774859+SBerdnikau@users.noreply.github.com>
2020-08-04 16:53:01 +0300

    day 3

---
+++ landingpage-exe/app/js/common.js
@@ -0,0 +1,37 @@
+//
+const arrow = document.querySelector(".arrow");
+
+arrow.addEventListener("click", (e) => {
+    e.preventDefault();
+    e.target.closest(".panel-left-language").querySelector(".panel-left-language-list").classList
+        .toggle('panel-left-language-list--active');
+
+});
+
+
+ // Initialize Swiper
+// const muSwiper = new Swiper('.swiper-container', {
+//     pagination: {
+//         el: '.swiper-pagination-line',
+//         type: 'progressbar'
+//     },
+// });
+
+
+ const sefaultSwiper = new Swiper('.swiper-container', {
+  pagination: {
+    el: '.swiper-pagination',
+    type: 'bullets',
+   // clickable: true
+   },
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+ autoplay: {
+     delay: 5000,
+     loop: true
+ },
+  speed: 400,
+  spaceBetween: 0
+});
